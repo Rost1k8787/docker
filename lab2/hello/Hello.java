e68380928dc6c553f72c5ae209a700f5ae487def
R0st1k8787 <maks.rastik.2405@gmail.com>
2024-01-24 17:39:47 +0200

    Made interactive

--- lab2/hello/Hello.java
+++ lab2/hello/Hello.java
@@ -1,5 +1,11 @@
-public class Hello { 
-    public static void main( String argv[] ) { 
-        System.out.println( "Hello, World" ); 
-    } 
-} 
+import import java.util.Scanner;
+public class Hello
+{
+    public static void main( String argv[] )
+    {
+        System.out.println("What's your name");
+        Scanner in = new Scanner(System.in);
+        String my_name = in.nextLine();
+        System.out.println( "Hello, " + my_name + "!" );
+    }
+}
